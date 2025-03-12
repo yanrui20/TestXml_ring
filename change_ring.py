aaff517a8d17bd4d1610e82186b2d95bc898046a
yanrui <1657066071@qq.com>
2025-03-12 19:59:17 +0800

    32GPUs_merge_sccl_sim_nccl

--- change_ring.py
+++ change_ring.py
@@ -3,6 +3,26 @@ from copy import deepcopy
 import os
 from pathlib import Path
 
+def get_new_tb(tb, tb_index, tb_start_index, chan, o_chunks):
+    new_tb = deepcopy(tb)
+    # 修改chan
+    new_tb.set('chan', str(chan)) # 合并的这一份chan都是1
+    # 修改id
+    new_tb.set('id', str(tb_index))
+    # 修改steps
+    for step in new_tb.findall('step'):
+        # 修改srcoff和dstoff
+        for attr in ['srcoff', 'dstoff']:
+            srcbuf = step.get("srcbuf")
+            if srcbuf == 'o':
+                value = int(step.get(attr))
+                step.set(attr, str(value + o_chunks * chan))
+        # 修改depid
+        depid = int(step.get("depid"))
+        if depid >= 0:
+            step.set("depid", str(depid + tb_start_index))
+    return new_tb
+
 def change_ring(input_file, output_file, ring):
     # 读取XML文件
     tree = ET.parse(input_file)
@@ -20,9 +40,64 @@ def change_ring(input_file, output_file, ring):
     # 保存修改后的文件
     tree.write(output_file, encoding='UTF-8', xml_declaration=False)
 
-if __name__ == '__main__':
-    ring_str = "0  7  6  5  4  3  2  1  9 10 11 12 13 14 15  8 16 23 22 21 20 19 18 17 25 26 27 28 29 30 31 24"
-    ring = [int(i) for i in ring_str.split()]
-    input = "/Users/yanrui/vscode/nccl/TestXml_ring/Neogen_AG/32GPUs/sccl_ring_1ch_16ins/test.xml"
-    output = "/Users/yanrui/vscode/nccl/TestXml_ring/Neogen_AG/32GPUs/sccl_ring_1ch_16ins_copy_nccl_ring/test.xml"
-    change_ring(input, output, ring)
+def merge_ring(inputs, output, instance):
+    tree1 = ET.parse(inputs[0])
+    root1 = tree1.getroot()
+
+    nchannels = int(root1.get("nchannels"))
+    root1.set("nchannels", str(nchannels * instance))
+    nchunksperloop = int(root1.get("nchunksperloop"))
+    root1.set("nchunksperloop", str(nchunksperloop * instance))
+
+    trees = [ET.parse(input) for input in inputs[1:]]
+    roots = [tree.getroot() for tree in trees]
+    for gpu in root1.findall('.//gpu'):
+        o_chunks = int(gpu.get('o_chunks'))
+        gpu.set('o_chunks', str(o_chunks*instance))
+        
+        original_tbs = [gpu.findall('tb')]
+        tb_index = len(original_tbs[0])
+        ## 先获得其他tree中对应gpu的tb
+        for root in roots:
+            tree_gpus = root.findall('.//gpu')
+            for gpu2 in tree_gpus:
+                if gpu2.get('id') == gpu.get('id'):
+                    original_tbs.append(gpu2.findall('tb'))
+        ## 按照次序合并tb
+        for chan in range(1, instance):
+            tb_start_index = tb_index
+            for tb in original_tbs[chan % len(original_tbs)]:
+                new_tb = get_new_tb(tb, tb_index, tb_start_index, chan, o_chunks)
+                tb_index += 1
+                gpu.append(new_tb)
+    
+    os.makedirs(os.path.dirname(output), exist_ok=True)
+    tree1.write(output, encoding='UTF-8', xml_declaration=False)
+
+def dump_base_rings():
+    ring_strs = [
+        "0 7 6 5 4 3 2 1   9 10 11 12 13 14 15 8   16 23 22 21 20 19 18 17   25 26 27 28 29 30 31 24",
+        "1 2 3 4 5 6 7 0   8 15 14 13 12 11 10 9   17 18 19 20 21 22 23 16   24 31 30 29 28 27 26 25",
+        "2 1 0 7 6 5 4 3   11 12 13 14 15 8 9 10   18 17 16 23 22 21 20 19   27 28 29 30 31 24 25 26",
+        "3 4 5 6 7 0 1 2   10 9 8 15 14 13 12 11   19 20 21 22 23 16 17 18   26 25 24 31 30 29 28 27",
+        "4 3 2 1 0 7 6 5   13 14 15 8 9 10 11 12   20 19 18 17 16 23 22 21   29 30 31 24 25 26 27 28",
+        "5 6 7 0 1 2 3 4   12 11 10 9 8 15 14 13   21 22 23 16 17 18 19 20   28 27 26 25 24 31 30 29",
+        "6 5 4 3 2 1 0 7   15 8 9 10 11 12 13 14   22 21 20 19 18 17 16 23   31 24 25 26 27 28 29 30",
+        "7 0 1 2 3 4 5 6   14 13 12 11 10 9 8 15   23 16 17 18 19 20 21 22   30 29 28 27 26 25 24 31",
+    ]
+    for i, ring_str in enumerate(ring_strs):
+        ring = [int(i) for i in ring_str.split()]
+        input = "/Users/yanrui/vscode/nccl/TestXml_ring/Neogen_AG/32GPUs/sccl_ring_1ch_1ins/test.xml"
+        output = f"/Users/yanrui/vscode/nccl/TestXml_ring/Neogen_AG/32GPUs_merge_sccl_sim_nccl/base_ring_index_{i}/test.xml"
+        os.makedirs(os.path.dirname(output), exist_ok=True)
+        change_ring(input, output, ring)
+
+if __name__ == "__main__":
+    # dump_base_rings()
+    dir_path = "/Users/yanrui/vscode/nccl/TestXml_ring/Neogen_AG/32GPUs_merge_sccl_sim_nccl"
+    base_ring = [0, 3]
+    inputs = [
+        f"{dir_path}/base_ring_index_{i}/test.xml" for i in base_ring
+    ]
+    output = f"{dir_path}/merged_ring_{'_'.join([str(i) for i in base_ring])}/test.xml"
+    merge_ring(inputs=inputs, output=output, instance=8)
