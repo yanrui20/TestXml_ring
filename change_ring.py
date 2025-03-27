add5983c583651b183a89df932947fca4ba4d057
yanrui <1657066071@qq.com>
2025-03-27 14:22:20 +0800

    最佳9ring

--- change_ring.py
+++ change_ring.py
@@ -154,10 +154,10 @@ if __name__ == "__main__":
     # dump_base_rings()
     dir_path = "./Neogen_AG/32GPUs_sequence_test"
     base_ring = [0, 1, 2, 3, 4, 5, 6, 7]
-    instance = 48
+    instance = 32
     inputs = [f"{dir_path}/base_ring_index_{i}/test.xml" for i in base_ring]
     inter_ring = [8]
-    inter_instance = 6
+    inter_instance = 4
     inter_inputs = [f"{dir_path}/base_ring_index_{i}/test.xml" for i in inter_ring]
     output = f"{dir_path}/merged_ring_{'_'.join([str(i) for i in base_ring])}_ins{instance}"
     if inter_ring:
