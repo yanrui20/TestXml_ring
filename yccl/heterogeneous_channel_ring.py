aa86fc0ae600de8cc20b3536cb795a42273be362
yanrui <1657066071@qq.com>
2025-04-15 16:31:44 +0800

    放弃odd2odd

--- yccl/heterogeneous_channel_ring.py
+++ yccl/heterogeneous_channel_ring.py
@@ -50,14 +50,3 @@ def heterogeneous_channel_ring(coll, dims, channels):
 
     
     return algo
- 
-if __name__ == "__main__":
-    dims = [8, 2]
-    channels = [32, 16]
-    root = heterogeneous_channel_ring(coll=AG, dims=dims, channels=channels)
-    # root.show_xml()
-    root.check()
-    gpus = math.prod(dims)
-    file = f"./yccl_AG/{gpus}GPUs/ring_{"_".join([str(i) for i in dims])}" \
-        f"_channel_{"_".join([str(i) for i in channels])}/yccl_test.xml"
-    root.store(file)
